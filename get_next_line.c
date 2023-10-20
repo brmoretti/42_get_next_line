b766925e1d59f36a1ac31aee10b323a1b41b3b13
Bruno Moretti <brmoretti@gmail.com>
2023-10-20 00:36:38 -0300

    buffer leftovers

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,52 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:25:38 by bmoretti          #+#    #+#             */
-/*   Updated: 2023/10/19 14:09:00 by bmoretti         ###   ########.fr       */
+/*   Updated: 2023/10/20 00:07:24 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h> //to remove
 #include <strings.h> //to remove
-#define BUFFER_SIZE 500 //to remove
+#define BUFFER_SIZE 30 //to remove
 
-void	ft_position(size_t *pos)
+char	*ft_buffer_leftover(size_t *i, char *buffer, size_t lo_limit)
 {
-	static size_t	bytes_read;
+	size_t	j;
+	char	*b_leftover;
+	char	*ptr;
 
-
+	j = *i;
+	while (*i && *i < lo_limit && buffer[*i] != '\n')
+		(*i)++;
+	b_leftover = malloc((*i - j) + 1);
+	if (!b_leftover)
+		return (NULL);
+	ptr = b_leftover;
+	while (j < *i)
+		*(ptr++) = buffer[j++];
+	*ptr = '\0';
+	if (++(*i) == BUFFER_SIZE)
+		*i = 0;
+	printf("b_leftover: %s\n", b_leftover);
+	return(b_leftover);
 }
 
 char	*get_next_line(int fd)
 {
 	static char		buffer[BUFFER_SIZE];
-	ssize_t			bytes_read;
-	static ssize_t	total_bytes;
+	static size_t	i;
+	static ssize_t	n_bytes;
+	char			*line;
 
-	bzero(buffer, BUFFER_SIZE);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	total_bytes += bytes_read;
-	printf("bytes_read: %zu\n", bytes_read);
-	printf("total_bytes: %zu\n", total_bytes);
-	printf("buffer: %s\n", buffer);
+	line = ft_buffer_leftover(&i, buffer, n_bytes);
+	if (i || !line)
+		return (line);
+	n_bytes = read(fd, buffer, BUFFER_SIZE);
+	if (n_bytes < BUFFER_SIZE)
+		bzero(&buffer[n_bytes], BUFFER_SIZE - n_bytes - 1);
+	printf("fd: %p\n", &fd);
+	printf("n_bytes: %zu ", n_bytes);
+	printf("%s\n", buffer);
 	return (buffer);
 }
