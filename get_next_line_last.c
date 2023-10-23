232b1316980f516551ee4c56725d92a5f73c7643
Bruno Moretti <bmoretti@c2r5p6.42sp.org.br>
2023-10-23 20:01:17 -0300

    working

--- get_next_line_last.c
+++ get_next_line_last.c
@@ -6,7 +6,7 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 21:44:09 by bmoretti          #+#    #+#             */
-/*   Updated: 2023/10/23 14:42:21 by bmoretti         ###   ########.fr       */
+/*   Updated: 2023/10/23 19:44:05 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,16 @@
 #include <strings.h>   //to remove
 #define BUFFER_SIZE 20 // to remove
 
-size_t	ft_strlen(const char *s);
-
-ssize_t	ft_fill_buffer(int fd, char **buffer)
+size_t	ft_strlen(const char *s)
 {
-	ssize_t	size;
+	size_t	i;
 
-	if (!*buffer)
-		*buffer = calloc(BUFFER_SIZE + 1, 1);
-	if (!*buffer)
+	if (!s)
 		return (0);
-	size = read(fd, *buffer, BUFFER_SIZE);
-	if (!size)
-		free (*buffer);
-	return (size);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
 size_t	ft_strlen_till_bslash_n(const char *s)
@@ -41,12 +37,12 @@ size_t	ft_strlen_till_bslash_n(const char *s)
 	while (s[i])
 	{
 		if (s[++i - 1] == '\n')
-			break;
+			break ;
 	}
 	return (i);
 }
 
-char	*ft_join_till_bslash_n(char	*dst, char *src)
+char	*ft_join_till_bslash_n(char *dst, char *src)
 {
 	ssize_t	i;
 	ssize_t	j;
@@ -57,58 +53,93 @@ char	*ft_join_till_bslash_n(char	*dst, char *src)
 		return (NULL);
 	i = 0;
 	while (dst && dst[i])
-		joined[i] = dst[i++];
-	j = 0;
+	{
+		joined[i] = dst[i];
+		i++;
+	}
 	if (dst)
-		free (dst);
+		free(dst);
+	j = 0;
 	while (src && src[j])
 	{
 		joined[i + j] = src[j];
 		if (src[++j - 1] == '\n')
-			break;
+			break ;
 	}
 	joined[i + j] = '\0';
 	return (joined);
 }
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	char	*occ;
-
-	occ = NULL;
 	while (*s)
-	{
 		if (*s++ == (unsigned char)c)
-			occ = (char *)(s - 1);
-	}
-	if (!(unsigned char)c)
-		occ = (char *)(s);
-	return (occ);
+			return ((char *)--s);
+	if ((unsigned char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+void	ft_move_to_begin(char *to, char *from)
+{
+	while (*from)
+		*(to++) = *(from++);
+	while (*to)
+		(*to++) = '\0';
 }
 
-char	*ft_line_recursive(char **line, char **buffer, ssize_t read_size)
+char	*ft_line_recursive(int fd, char **line, char *buffer, ssize_t read_size)
 {
 	char	*slash_n;
 
-	if (!read_size)
-		return (NULL);
-	*line = ft_join_till_bslash_n(*line, *buffer);
+	*line = ft_join_till_bslash_n(*line, buffer);
 	if (!*line)
 		return (NULL);
-	slash_n = ft_strrchr(*buffer, '\n');
+	slash_n = ft_strchr(buffer, '\n');
 	if (slash_n)
-		*buffer = ft_join_till_bslash_n(*buffer, )
+	{
+		ft_move_to_begin(buffer, slash_n + 1);
+		return (*line);
+	}
+	bzero(buffer, BUFFER_SIZE + 1);
+	read_size = read(fd, buffer, BUFFER_SIZE);
+	if (read_size)
+		*line = ft_line_recursive(fd, line, buffer, read_size);
+	return (*line);
+}
+
+char	*ft_line_iterative(int fd, char **line, char *buffer, ssize_t read_size)
+{
+	char	*slash_n;
+
+	while (1)
+	{
+		*line = ft_join_till_bslash_n(*line, buffer);
+		if (!*line)
+			return (NULL);
+		slash_n = ft_strchr(buffer, '\n');
+		if (slash_n)
+		{
+			ft_move_to_begin(buffer, slash_n + 1);
+			return (*line);
+		}
+		bzero(buffer, BUFFER_SIZE + 1);
+		read_size = read(fd, buffer, BUFFER_SIZE);
+		if (read_size)
+			*line = ft_line_recursive(fd, line, buffer, read_size);
+	}
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	buffer[BUFFER_SIZE + 1];
 	ssize_t		read_size;
 	char		*line;
 
-	if (!buffer || !*buffer)
-		read_size = ft_fill_buffer(fd, &buffer);
+	if (!*buffer)
+		read_size = read(fd, buffer, BUFFER_SIZE);
+	if (!read_size)
+		return (NULL);
 	line = NULL;
-	line = ft_line_recursive(&line, buffer, read_size);
+	line = ft_line_iterative(fd, &line, buffer, read_size);
 	return (line);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 11:24:15 by bmoretti          #+#    #+#             */
-/*   Updated: 2023/10/21 19:22:35 by bmoretti         ###   ########.fr       */
+/*   Updated: 2023/10/23 19:42:53 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,27 @@ int	main(void)
 	int		n_files;
 	char	*line;
 	int		i;
-	char	fl_name[] = "text0.txt";
+	char	fl_name[] = "text5.txt";
 
 	n_files = 2;
 	fds = malloc(sizeof(int) * n_files);
 	i = -1;
 	while (++i < n_files)
 	{
-		fl_name[4] = i + '0';
+		//fl_name[4] = i + '0';
 		fds[i] = open(fl_name, O_RDONLY);
 		if (fds[i] == -1)
 		{
 			printf("Error opening file %s\n", fl_name);
 			return (1);
 		}
-		printf("File %s opened with fd %d\n", fl_name, fds[i]);
+		//printf("File %s opened with fd %d\n", fl_name, fds[i]);
 	}
 	i = -1;
-	while (i++ < 13)
+	while (i++ < 16)
 	{
 		line = get_next_line(fds[0]);
-		printf("%d: %s$\n", i, line);
+		printf("%d: %s", i, line);
 		free(line);
 	}
 	free(fds);
